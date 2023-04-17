5988dc2ca231b7b9d1226895f058c2893bcf794c
Нисик <@DESKTOP-ALORV4C>
2023-04-17 14:20:43 +0300

    Добавьте файлы проекта.

---
+++ day 1 (6)/Program.cs
@@ -0,0 +1,17 @@
+﻿int n = 18; // здесь можно изменить число заданий
+
+string description;
+if (n % 10 == 1 && n != 11)
+{
+    description = $"{n} учебное задание";
+}
+else if (n % 10 >= 2 && n % 10 <= 4 && (n < 10 || n > 20))
+{
+    description = $"{n} учебных задания";
+}
+else
+{
+    description = $"{n} учебных заданий";
+}
+
+Console.WriteLine(description);
